cc60be912d98c6b636fc5c3d03a353077776ae74
rahul <rg4005450@gmail.com>
2024-04-08 11:43:14 +0530

    Commit 3

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,15 +4,21 @@ import toast from "react-hot-toast";
 
 const Input = () => {
   const [title, setTitle] = useState("");
-  const [languages, setLanguages] = useState([]);
+    const [selectLanguage, setSelectLanguage] = useState(null)
+
+    let languages = []
 
   useEffect(() => {
-    setLanguages(window.speechSynthesis.getVoices());
+    languages  = window.speechSynthesis.getVoices();
   }, []);
 
   const onClick = () => {
     if (!title) {
       toast.error("Please Provide name");
+      return;
+    }
+    if (!selectLanguage) {
+      toast.error("Please Select A language");
       return;
     }
     const msg = new SpeechSynthesisUtterance();
@@ -30,9 +36,6 @@ const Input = () => {
         </h1>
       </div>
       <div className="flex justify-center items-center flex-col h-full">
-        <h1 className="text-white text-5xl mb-5 font-bold text-center">
-          Enter Text
-        </h1>
         <textarea
           cols={"50"}
           rows={"4"}
@@ -48,15 +51,19 @@ const Input = () => {
           Available Languages
         </h1>
         <div className="grid grid-cols-6 grid-rows-2">
-        {
-            languages && languages.map((item,index)=>(
-                <div className="bg-purple-700 h-[12vh] w-[12rem] mx-2 my-2 cursor-pointer">
-                    
-                    Name - {item.name}
-                    Language Code - {item.code}
-                </div>
-            ))
-        }
+          {languages &&
+            languages.map((item, index) => (
+              <div
+                className="bg-transparent text-white border border-purple-400 px-3 h-[12vh]  mx-2 my-2 cursor-pointer flex flex-col justify-center items-center"
+                key={index}
+                onClick={()=>{
+                    setSelectLanguage(item);
+                }}
+              >
+                <p>Name - {item.name}</p>
+                <p>Language Code - {item.lang}</p>
+              </div>
+            ))}
         </div>
         <button
           className="mt-4 bg-purple-700 border border-purple-700 rounded-xl px-7 py-3 outline-none text-white hover:bg-gray-200 hover:text-purple-700"
